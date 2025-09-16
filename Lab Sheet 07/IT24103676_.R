5bbd80d9ebf27c4f455bf7bffa731645d51ec495
IT24103676 <it24103676@my.sliit.lk>
2025-09-16 11:29:52 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103676_.R
@@ -0,0 +1,18 @@
+setwd('C:\\Users\\HP\\Desktop\\IT24103676_')
+getwd()
+
+#Q1
+punif(25, min=0, max=40)-punif(10, min=0, max=40)
+
+#Q2
+lambda<-1/3
+pexp(2,lambda)
+
+#Q3
+#i
+mean<-100
+standard_deviation<-15
+1-pnorm(130,mean,standard_deviation)
+
+#ii
+qnorm(0.95,mean,standard_deviation)
